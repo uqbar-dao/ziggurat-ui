9541bf567570e9b26a039a2a2bc31ebf5222547b
0x70b1a5 <0x70b1a5@users.noreply.github.com>
2022-10-10 12:12:58 -0400

    fix #52 (#56)

--- src/components-zig/tests/TestResultDisplay.tsx
+++ src/components-zig/tests/TestResultDisplay.tsx
@@ -9,6 +9,8 @@ import Modal from '../../components/popups/Modal';
 import Entry from '../../components/spacing/Entry';
 import Field from '../../components/spacing/Field';
 import { STATUS_CODES_RAW } from '../../utils/constants';
+import Divider from '../../components/spacing/Divider';
+import HexNum from '../../components/text/HexNum';
 
 const TestResultDisplay = ({ result, expectedError }: { result?: TestResult, expectedError: number }) => {
   const [showAllResultsModal, setShowAllResultsModal] = useState(false)
@@ -19,38 +21,46 @@ const TestResultDisplay = ({ result, expectedError }: { result?: TestResult, exp
         <Text ml1>{String(result?.fee)}</Text>
       </Field>
       {!!result && result.success === false && (
-        <Field name='Failures'>
-          <Col>
-            {result?.errorcode !== expectedError && (
-              <Field name='Status Code'>{`expected: ${STATUS_CODES_RAW[expectedError]}, got: ${STATUS_CODES_RAW[result.errorcode]}`} </Field>
-            )}
-            {Object.keys(result?.grains).filter(id => result.grains[id].match === false)
-              .map(id => {
-                if (result.grains[id].expected && !result.grains[id].made) {
-                  return (
-                    <Entry divide key={id}>
-                      <Field name='ID'>{displayPubKey(id)} </Field>
-                      <Field name='Error'>No grain matching that expectation</Field>
-                    </Entry>
-                  )
-                }
-                const diff = getGrainDiff(result.grains[id].expected, result.grains[id].made)
+        <Entry>
+          {result?.errorcode !== expectedError && (
+            <Field name='Status Code'>{`expected: ${STATUS_CODES_RAW[expectedError]}, got: ${STATUS_CODES_RAW[result.errorcode]}`} </Field>
+          )}
+          {Object.keys(result?.grains)
+            .filter(id => !result.grains[id].match)
+            .map((id, index, arr) => {
+              const grain = result.grains[id]
+              const errorMsg = (grain.expected === null && grain.match === null) 
+                ? 'Grain was not expected to change.' 
+                : (grain.expected && !grain.made)
+                  ? 'Grain changes did not match expectations.'
+                  : ''
+
+              if (errorMsg) {
                 return (
-                  <Entry divide key={id}>
-                    <Field name='ID'>{displayPubKey(id)} </Field>
-                    {Object.keys(diff).map(field => (
-                      <Entry key={field}>
-                        <Field name='Field'>{field} </Field>
-                        <Field name='Expected'>{String(diff[field].expected)} </Field>
-                        <Field name='Actual'>{String(diff[field].result)} </Field>
-                      </Entry>
-                    ))}
+                  <Entry className='ml1' divide={index < arr.length - 1} key={id}>
+                    <h4>Fail #{index+1}</h4>
+                    <Field name='ID'> <HexNum num={id} displayNum={displayPubKey(id)} copy copyText={id} /></Field>
+                    <Field name='Error'>{errorMsg}</Field>
                   </Entry>
                 )
-              })
-            }
-          </Col>
-        </Field>
+              }
+              
+              const diff = getGrainDiff(grain.expected, grain.made)
+              return (
+                <Entry divide key={id}>
+                  <Field name='ID'>{displayPubKey(id)} </Field>
+                  {Object.keys(diff).map(field => (
+                    <Entry key={field}>
+                      <Field name='Field'>{field} </Field>
+                      <Field name='Expected'>{String(diff[field].expected)} </Field>
+                      <Field name='Actual'>{String(diff[field].result)} </Field>
+                    </Entry>
+                  ))}
+                </Entry>
+              )
+            })
+          }
+        </Entry>
       )}
       <Button style={{ margin: '8px 0 0 16px' }} onClick={() => setShowAllResultsModal(true)} small>Show full test output</Button> 
       <Modal title='Full Test Results' show={showAllResultsModal} hide={() => setShowAllResultsModal(false)}>
