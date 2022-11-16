5aeb699124d3799320768bcd2fa55b74069f53e2
Tobias Merkle <tgamerkle@gmail.com>
2022-11-16 14:30:20 -0500

    json links for to/from and item

--- src/components/text/Json.tsx
+++ src/components/text/Json.tsx
@@ -4,14 +4,31 @@ import './Json.scss'
 
 interface JsonProps extends React.HTMLAttributes<HTMLPreElement> {
   json: any
+  doNotLinkifyAddresses?: boolean
   options?: FormatSettings
 }
 
-const Json: React.FC<JsonProps> = ({ json, className = '', options, ...props }) => {
+const Json: React.FC<JsonProps> = ({ json, doNotLinkifyAddresses, className = '', options, ...props }) => {
+  const linkifyHtml = (json: any) => {
+    let out = prettyPrintJson.toHtml(json, options)
+    const tofrom_re = '(<span class=json-key>(to|from)</span>\
+\\n?\\r?\\s*<span class=json-mark>: </span>\
+\\n?\\r?\\s*<span class=json-string>")(.+?)("</span>)'
+    const item_re = '(<span class=json-key>item</span>\
+\\n?\\r?\\s*<span class=json-mark>: </span>\
+\\n?\\r?\\s*<span class=json-string>")(.+?)("</span>)'
+    out = out.replace(new RegExp(tofrom_re, 'g'), '$1<a href="/apps/ziggurat/indexer/address/$3">$3</a>$4')
+      .replace(new RegExp(item_re, 'g'), '$1<a href="/apps/ziggurat/indexer/item/$2">$2</a>')
+    return out
+  }
+
+  const final = doNotLinkifyAddresses ? prettyPrintJson.toHtml(json, options) 
+    : linkifyHtml(json)
+  
   return (
     <pre className={`json-container ${className}`} 
       {...props} 
-      dangerouslySetInnerHTML={{ __html: prettyPrintJson.toHtml(json, options)  }} 
+      dangerouslySetInnerHTML={{ __html: final }} 
     />
   )
 }
